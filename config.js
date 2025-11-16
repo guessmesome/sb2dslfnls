7793b6a1469ac6d5f9a29f1cbe3f5ea261a49544
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-16 12:26:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     products: {
-        1: 'https://vldmnt.ru/go/sz1ntqfdba',
+        1: 'https://vldmnt.ru/go/s7khvqyv6g',
         2: 'https://vldmnt.ru/go/sx3vh0reuu',
         3: 'https://vldmnt.ru/go/s7tjlffjq9',
         4: 'https://vldmnt.ru/go/slu6f20oi4'
